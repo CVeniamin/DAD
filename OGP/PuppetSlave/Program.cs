a49587ce21e46f06db093b814de7f78a2302edc6
thexeos <thexeos@users.noreply.github.com>
2017-10-22 23:33:33 +0100

    Project structure

---
+++ OGP/PuppetSlave/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuppetSlave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
